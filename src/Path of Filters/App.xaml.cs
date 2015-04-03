e7350a4e5645e423f88c7eb9a9b5150dd66a06df
M1nistry <Ministry@live.com.au>
2015-04-03 15:29:00 +1100

    Initial Commint. Going WPF.

---
+++ src/Path of Filters/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Path_of_Filters
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
